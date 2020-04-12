f1397db9bae0883ccbec2d1a52cabafbce46f251
Sanley5 <63549207+Sanley5@users.noreply.github.com>
2020-04-12 11:36:25 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -3,3 +3,25 @@ public class Aluno{
  private int id;
 
 }
+
+public ALuno (String nome, int id) {
+        this.nome = nome;
+        this.id= id;
+    }
+ 
+    public String getNome() {
+     return nome; 
+    }
+ 
+    public int getId() {
+     return id; 
+    }
+ 
+    public void setNome(String x) {
+     this.nome = nome; 
+    }
+ 
+    public void setId(int id) {
+     this.id = id; 
+    } 
+}
